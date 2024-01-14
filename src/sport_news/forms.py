f3f49510dcaa5c543a56700013cb97a75de7a40e
Victor952 <98576613+Victor952@users.noreply.github.com>
2024-01-15 01:27:35 +0300

    third commit

---
+++ src/sport_news/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from sport_news.models import City, Sport_type
+class FindForm(forms.Form):
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(), to_field_name='slug',
+        required=False, label='Город',
+        widget=forms.Select(attrs={'class': 'form-control'}))
+    sport_type = forms.ModelChoiceField(
+        queryset=Sport_type.objects.all(), to_field_name='slug',
+        required=False, label='Специальность',
+        widget=forms.Select(attrs={'class': 'form-control'}))
--- src/sport_news/views.py
+++ src/sport_news/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from .forms import FindForm
 from .models import News
 def home_view(request):
+    form = FindForm()
     city = request.GET.get('city')
     sport_type = request.GET.get('sport_type')
     qs = []
     if city or sport_type:
         _filter = {}
         if city:
-            _filter['city__name'] = city
+            _filter['city__slug'] = city
         if sport_type:
-            _filter['sport_type__name'] = sport_type
-    qs = News.objects.filter(**_filter)
-    return render(request, 'sport_news/home.html', {'object_list': qs})
+            _filter['sport_type__slug'] = sport_type
+        qs = News.objects.filter(**_filter)
+    return render(request, 'sport_news/home.html',
+                  {'object_list': qs, 'form': form})
