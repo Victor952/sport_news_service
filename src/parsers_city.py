1bcdad8aacc1590df5a1b1eee8f0f51f45ac402c
Victor952 <98576613+Victor952@users.noreply.github.com>
2024-02-17 01:25:49 +0300

    own commit: виды спорта - в виде плиток, выбор города - справа

--- src/parsers_city.py
+++ src/parsers_city.py
@@ -3,11 +3,14 @@ import codecs
 from bs4 import BeautifulSoup as BS
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
            'Accept': 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8'}
+'''h = codecs.open('krskbezformata.html', 'w', 'utf-8')
+h.write(str(resp.text))
+h.close()'''
 def sportuspro(url):
     news = []
     errors = []
-    domain = 'https://sportus.pro/'
     resp = requests.get(url, headers=headers)
+    domain = 'https://sportus.pro/'
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
         main_div = soup.find('div', class_='row-no-gutter')
@@ -26,9 +29,30 @@ def sportuspro(url):
     else:
         errors.append({'url': url, 'title': 'Page don’t response'})
     return news, errors
+def bezformata(url):
+    news = []
+    errors = []
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        main_section = soup.find('section', class_='listtopicbox')
+        if main_section:
+            article_lst = main_section.find_all('article', class_='listtopicline')
+            for article in article_lst:
+                before_title = article.find('a', attrs={'itemprop': 'url'})
+                title = before_title.find('h3')
+                href = article.a['href']
+                description = article.find('div', attrs={'itemprop': 'description'})
+                news.append({'url': href, 'title': title.text,
+                             'description': description.text})
+        else:
+            errors.append({'url': url, 'title': 'Div doesn’t exists'})
+    else:
+        errors.append({'url': url, 'title': 'Page don’t response'})
+    return news, errors
 if __name__ == '__main__':
-    url = 'https://sportus.pro/news/'
-    news, errors = sportuspro(url)
-    h = codecs.open('sportuspro.txt', 'w', 'utf-8')
+    url = 'https://krasnoyarsk.bezformata.com/sport/'
+    news, errors = bezformata(url)
+    h = codecs.open('bezformata.txt', 'w', 'utf-8')
     h.write(str(news))
     h.close()
--- src/sport_news/forms.py
+++ src/sport_news/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from sport_news.models import City, Sport_type
-class FindForm(forms.Form):
+class FindForm_city(forms.Form):
     city = forms.ModelChoiceField(
         queryset=City.objects.all(), to_field_name='slug',
         required=False, label='Город',
         widget=forms.Select(attrs={'class': 'form-control'}))
+class FindForm_st(forms.Form):
     sport_type = forms.ModelChoiceField(
         queryset=Sport_type.objects.all(), to_field_name='slug',
         required=False, label='Вид спорта',
